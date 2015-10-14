d98b80bff004832a2371d7e053b6e1c0cc4ffe56
aximran <aliimran480@gmail.com>
2015-10-13 22:30:15 -0500

    dele

--- delegateexample/Program.cs
+++ delegateexample/Program.cs
@@ -2,13 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+delegate int NumberChanger(int n);
 namespace delegateexample
 {
-    class Program
+    class TestDelegate
     {
+        static int num = 7;
+        public static int AddNum(int p)
+        {
+            num += p;
+            return num;
+        }
+
+        public static int MultNum(int q)
+        {
+            num *= q;
+            return num;
+        }
+        public static int getNum()
+        {
+            return num;
+        }
+
         static void Main(string[] args)
         {
+            //create delegate instances
+            NumberChanger nc1 = new NumberChanger(AddNum);
+            NumberChanger nc2 = new NumberChanger(MultNum);
+
+            //calling the methods using the delegate objects
+            nc1(25);
+            Console.WriteLine("Value of Num: {0}", getNum());
+            nc2(5);
+            Console.WriteLine("Value of Num: {0}", getNum());
+            Console.ReadKey();
         }
     }
 }
